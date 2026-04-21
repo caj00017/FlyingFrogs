d7890dcd7cbb95bbcd1b0757fe8fca63c46f9a07
Oliver Lay <oklay252@gmail.com>
2026-04-21 15:06:06 -0400

    removal of merge conflicts

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 // render home page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/login.html');
 });
 
 ///// API ROUTES /////
